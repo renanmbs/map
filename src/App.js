ff9365ca0b8fde84032853ba0eb5e62203e23b9c
renanmbs <mougenot.renan@gmail.com>
2026-01-05 18:01:28 -0700

    Jan 5

--- src/App.js
+++ src/App.js
@@ -1,29 +1,3 @@
-// import './App.css';
-// import monarchLogo from './images/Monarch3Logo.svg';
-// import SalesTerritoryMap from "./sales_map/SalesTerritoryMap"
-// import ShipTerritoryMap from './ship_map/ship_map';
-
-// function App() {
-//   return (
-//     <div className="App">
-
-//       <div className="stm-header">
-
-//         <img src={monarchLogo} alt='Monarch Logo' className='stm-logo' />
-
-//         <div className="stm-title-search">
-//           <p>Sales Representative by State</p>
-
-//         </div>
-//       </div>
-//       <SalesTerritoryMap svgPath="/usa.svg" />
-//       <ShipTerritoryMap svgPath="/ship.svg" />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import './App.css';
 import MapContainer from "./MapContainer";
 
